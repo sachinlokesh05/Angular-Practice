f81a94ac6d69db69f0dc10ade0e08e128c81baf4
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-09-18 10:10:01 +0530

    [sachin]Add.added component for choose type

--- src/app/components/choose-type/choose-type.component.ts
+++ src/app/components/choose-type/choose-type.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-choose-type',
   templateUrl: './choose-type.component.html',
-  styleUrls: ['./choose-type.component.css']
+  styleUrls: ['./choose-type.component.scss']
 })
 export class ChooseTypeComponent implements OnInit {
 
