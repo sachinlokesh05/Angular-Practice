b89ec09d219719ed95b81fb2fabf8965dea3aec5
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-09-24 07:50:19 +0530

    [sachin]Add.added the conversion table component

--- src/app/components/choose-type/choose-type.component.ts
+++ src/app/components/choose-type/choose-type.component.ts
@@ -9,10 +9,39 @@ export class ChooseTypeComponent implements OnInit {
 
   public x = false;
   public y = false;
+  public z = false;
   constructor() {
    }
 
+  selectedlength(): void{
+    if ( this.x !== true) {
+      this.x = true;
+      this.y = false;
+      this.z = false;
+    }else{
+      this.x = false;
+    }
+  }
 
+  selectedtemp(): void{
+    if ( this.y !== true) {
+      this.y = true;
+      this.x = false;
+      this.z = false;
+    }else{
+      this.y = false;
+    }
+  }
+
+  selectedvolumne(): void{
+    if ( this.z !== true) {
+      this.z = true;
+      this.y = false;
+      this.x = false;
+    }else{
+      this.z = false;
+    }
+  }
 
   ngOnInit(): void {
   }
